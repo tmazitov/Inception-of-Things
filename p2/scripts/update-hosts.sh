f54be2f13e81e70131c73c72c136b5a3f09ea480
twillian <twillian.sc21@gmail.com>
2026-08-15 16:36:16 +0400

    upd : use old version

--- p2/scripts/update-hosts.sh
+++ p2/scripts/update-hosts.sh
@@ -2,18 +2,20 @@
 
 set -e
 
+K3S_IP="192.168.56.110"
+
 echo "Updating /etc/hosts..."
 
-for HOST in app1.com app2.com app3.com; do
+sudo sed -i '/# K3S-APPS-BEGIN/,/# K3S-APPS-END/d' /etc/hosts
 
-    # Remove previous entry
-    sed -i "/[[:space:]]${HOST}$/d" /etc/hosts
-
-    # Point domain to local machine
-    echo "127.0.0.1 ${HOST}" >> /etc/hosts
-
-done
+sudo tee -a /etc/hosts > /dev/null <<EOF
+# K3S-APPS-BEGIN
+${K3S_IP} app1.com
+${K3S_IP} app2.com
+${K3S_IP} app3.com
+# K3S-APPS-END
+EOF
 
 echo
-echo "Current application entries:"
-grep -E 'app1\.com|app2\.com|app3\.com' /etc/hosts
+echo "K3s host entries:"
+grep -A3 'K3S-APPS-BEGIN' /etc/hosts
